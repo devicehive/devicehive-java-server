e165237e67fd5e417b5d3196f76b44683cc0740b
nloboda <nikolay.loboda@dataart.com>
2013-07-20 21:56:43 +0400

    Added DeviceClassService

---
+++ src/main/java/com/devicehive/service/DeviceClassService.java
@@ -0,0 +1,16 @@
+package com.devicehive.service;
+
+import com.devicehive.model.DeviceClass;
+
+import javax.ejb.Stateless;
+import java.util.List;
+
+/**
+ * @author Nikolay Loboda
+ * @since 19.07.13
+ */
+@Stateless
+public class DeviceClassService {
+
+
+}
