659a5d21134f51f2abd7bf00a7e198d6baa73ccf
Anton Krasikov <krasikov@gmail.com>
2013-07-21 14:44:50 +0100

    Stubs for Device API.

--- src/main/java/com/devicehive/controller/DeviceController.java
+++ src/main/java/com/devicehive/controller/DeviceController.java
@@ -1,15 +1,43 @@
 package com.devicehive.controller;
 
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
 /**
  * TODO JavaDoc
  */
-@Path("/device")
 public class DeviceController {
 
+    @GET
+    @Path("/device")
     public Response getDeviceList() {
+        return Response.ok().build();
+    }
+
+    @GET
+    @Path("/device/{id}")
+    public Response getDevice() {
+        return Response.ok().build();
+    }
+
+    @PUT
+    @Path("/device/{id}")
+    public Response registerDevice() {
+        return Response.ok().build();
+    }
+
+    @DELETE
+    @Path("/device/{id}")
+    public Response deleteDevice() {
+        return Response.ok().build();
+    }
+
+    @GET
+    @Path("/device/{id}/equipment")
+    public Response getDeviceEquipment() {
         return Response.ok().build();
     }
 }
