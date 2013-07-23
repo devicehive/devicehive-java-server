7e2261e61578354d7226797134eae651087ba4f8
Anton Krasikov <krasikov@gmail.com>
2013-07-23 16:05:16 +0100

    0015845 - Basic auth is requested now
    When the client is not authenticated yet, a 401 WWW-Authorization header
    is sent back.

--- src/main/java/com/devicehive/auth/RolesAllowedFilter.java
+++ src/main/java/com/devicehive/auth/RolesAllowedFilter.java
@@ -1,17 +1,15 @@
 package com.devicehive.auth;
 
-
 import javax.annotation.Priority;
-import javax.ws.rs.ForbiddenException;
 import javax.ws.rs.Priorities;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
-
 
 @Priority(Priorities.AUTHORIZATION)
 public class RolesAllowedFilter implements ContainerRequestFilter {
@@ -19,7 +17,7 @@ public class RolesAllowedFilter implements ContainerRequestFilter {
     private final Set<String> allowedRoles;
 
     public RolesAllowedFilter(Collection<String> allowedRoles) {
-        this.allowedRoles = new HashSet<String>(allowedRoles);
+        this.allowedRoles = new HashSet<>(allowedRoles);
     }
 
     @Override
@@ -30,6 +28,10 @@ public class RolesAllowedFilter implements ContainerRequestFilter {
                 return;
             }
         }
-        throw new ForbiddenException();
+        requestContext.abortWith(Response
+                .status(Response.Status.UNAUTHORIZED)
+                .header("WWW-Authenticate", "Basic realm=\"devicehive\"")
+                .entity("{message:\"Not authorized\"}")
+                .build());
     }
 }
