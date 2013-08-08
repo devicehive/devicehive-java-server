8982661e9f5484b831fd6ce539c871f365632116
Julia Kulagina <Julia.Kulagina@dataart.com>
2013-08-08 17:42:15 +0400

    deleted unuseful

--- src/main/java/com/devicehive/configuration/Constants.java
+++ src/main/java/com/devicehive/configuration/Constants.java
@@ -4,8 +4,8 @@ public class Constants {
 
     public static final String PERSISTENCE_UNIT = "devicehive";
 
-    public static final String WEBSOCKET_SERVER_URL = "WebSocketServerUrl";
+    public static final String WEBSOCKET_SERVER_URL = "webSocketServerUrl";
 
-    public static final String REST_SERVER_URL = "RestServerUrl";
+    public static final String REST_SERVER_URL = "restServerUrl";
 
 }
