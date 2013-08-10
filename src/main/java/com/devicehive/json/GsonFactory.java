947b31892b7b70035d188b66854be84447c7f6b4
Stanislav Sidorenko <mail@stanislavsidorenko.com>
2013-08-10 10:36:06 +0400

    Gson's instances are now cached

--- src/main/java/com/devicehive/json/GsonFactory.java
+++ src/main/java/com/devicehive/json/GsonFactory.java
@@ -10,26 +10,36 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import java.sql.Timestamp;
+import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 import static com.devicehive.json.strategies.JsonPolicyDef.Policy;
 
 public class GsonFactory {
 
+    private static Map<Policy, Gson> cache = new ConcurrentHashMap<>();
+    private static Gson gson = createGsonBuilder().create();
+
     public static Gson createGson() {
-        return createGsonBuilder()
-                .create();
+        return gson;
     }
 
     public static Gson createGson(Policy policy) {
-        return createGsonBuilder()
+        Gson gson = cache.get(policy);
+        if (gson != null) {
+            return gson;
+        }
+        gson = createGsonBuilder()
                 .addDeserializationExclusionStrategy(new AnnotatedStrategy(policy))
                 .addSerializationExclusionStrategy(new AnnotatedStrategy(policy))
                 .create();
+        cache.put(policy, gson);
+        return gson;
     }
 
 
-    public static GsonBuilder createGsonBuilder() {
+    private static GsonBuilder createGsonBuilder() {
         return new GsonBuilder()
                 .setPrettyPrinting()
                 .serializeNulls()
