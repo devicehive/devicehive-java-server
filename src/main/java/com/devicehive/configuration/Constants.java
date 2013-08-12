ede6e75355b6bcf56951c1a1049a2aa233fea970
Stanislav Sidorenko <mail@stanislavsidorenko.com>
2013-08-12 19:07:59 +0400

    Minor fixes

--- src/main/java/com/devicehive/configuration/Constants.java
+++ src/main/java/com/devicehive/configuration/Constants.java
@@ -8,11 +8,11 @@ public class Constants {
 
     public static final String REST_SERVER_URL = "restServerUrl";
 
-    public static Long DEVICE_NOTIFICATION_NULL_ID_SUBSTITUTE = -1L;
+    public static final long DEVICE_NOTIFICATION_NULL_ID_SUBSTITUTE = -1L;
 
-    public final static String DEFAULT_WAIT_TIMEOUT = "30";
+    public static final String DEFAULT_WAIT_TIMEOUT = "30";
 
-    public final static long MAX_WAIT_TIMEOUT = 60L;
+    public static final long MAX_WAIT_TIMEOUT = 60L;
 
     public static final int MAX_LOGIN_ATTEMPTS = 10;
 
