7165369731328ad3fbd8af0872600bc8dba2c044
Stanislav Sidorenko <mail@stanislavsidorenko.com>
2013-08-29 11:05:08 +0400

    Build fix

--- server/src/main/java/com/devicehive/servlet/InfoServlet.java
+++ server/src/main/java/com/devicehive/servlet/InfoServlet.java
@@ -15,8 +15,11 @@ import java.io.IOException;
 @WebServlet("/index")
 public class InfoServlet extends HttpServlet{
 
+
+    private static final long serialVersionUID = -4886819685195322L;
+
     @EJB
-    private ConfigurationService configurationService;
+    private transient ConfigurationService configurationService;
 
     private static final String INFO_PAGE = "info_page.jsp";
 
