a5bd87957a95fc49a34d162df75ff809c9c4ef5e
Stanislav Sidorenko <mail@stanislavsidorenko.com>
2013-10-14 08:08:30 +0400

    Muliple authentication is now not allowed

--- server/src/main/java/com/devicehive/auth/websockets/AuthenticationInterceptor.java
+++ server/src/main/java/com/devicehive/auth/websockets/AuthenticationInterceptor.java
@@ -5,6 +5,7 @@ import com.devicehive.model.Device;
 import com.devicehive.service.DeviceService;
 import com.devicehive.util.ThreadLocalVariablesKeeper;
 import com.devicehive.websockets.handlers.annotations.WebsocketController;
+import com.devicehive.websockets.util.WebsocketSession;
 import com.google.gson.JsonObject;
 
 import javax.annotation.Priority;
@@ -12,6 +13,7 @@ import javax.ejb.EJB;
 import javax.interceptor.AroundInvoke;
 import javax.interceptor.Interceptor;
 import javax.interceptor.InvocationContext;
+import javax.websocket.Session;
 
 @WebsocketController
 @Interceptor
@@ -23,19 +25,24 @@ public class AuthenticationInterceptor {
 
     @AroundInvoke
     public Object authenticate(InvocationContext ctx) throws Exception {
-        JsonObject request = ThreadLocalVariablesKeeper.getRequest();
-        String deviceId = null, deviceKey = null;
-        if (request.get("deviceId") != null) {
-            deviceId = request.get("deviceId").getAsString();
-        }
-        if (request.get("deviceKey") != null) {
-            deviceKey = request.get("deviceKey").getAsString();
-        }
-        if (deviceId != null && deviceKey != null) {
-            Device device = deviceService.authenticate(deviceId, deviceKey);
-            if (device != null) {
-                HivePrincipal principal = new HivePrincipal(null, device, null);
-                ThreadLocalVariablesKeeper.setPrincipal(principal);
+        Session session = ThreadLocalVariablesKeeper.getSession();
+        if (WebsocketSession.getPrincipal(session) == null) {
+            ThreadLocalVariablesKeeper.setPrincipal(WebsocketSession.getPrincipal(session));
+        } else {
+            JsonObject request = ThreadLocalVariablesKeeper.getRequest();
+            String deviceId = null, deviceKey = null;
+            if (request.get("deviceId") != null) {
+                deviceId = request.get("deviceId").getAsString();
+            }
+            if (request.get("deviceKey") != null) {
+                deviceKey = request.get("deviceKey").getAsString();
+            }
+            if (deviceId != null && deviceKey != null) {
+                Device device = deviceService.authenticate(deviceId, deviceKey);
+                if (device != null) {
+                    HivePrincipal principal = new HivePrincipal(null, device, null);
+                    ThreadLocalVariablesKeeper.setPrincipal(principal);
+                }
             }
         }
         return ctx.proceed();
