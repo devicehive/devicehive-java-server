3f749fd9cd06d545254cbcd1591f12218b31e7cd
jkulagina <Julia.Kulagina@dataart.com>
2014-05-28 17:27:11 +0400

    Fix for global message bus (transactions fixed)

--- server/src/main/java/com/devicehive/messages/bus/GlobalMessageBus.java
+++ server/src/main/java/com/devicehive/messages/bus/GlobalMessageBus.java
@@ -14,10 +14,15 @@ import org.slf4j.LoggerFactory;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
-import javax.ejb.*;
+import javax.ejb.Asynchronous;
+import javax.ejb.ConcurrencyManagement;
+import javax.ejb.EJB;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
 import javax.enterprise.event.Observes;
 import javax.enterprise.event.TransactionPhase;
-import javax.inject.Inject;
 
 import static javax.ejb.ConcurrencyManagementType.BEAN;
 
@@ -30,23 +35,17 @@ import static javax.ejb.ConcurrencyManagementType.BEAN;
 public class GlobalMessageBus {
 
     private static final Logger logger = LoggerFactory.getLogger(GlobalMessageBus.class);
-
     private static final String DEVICE_COMMAND = "DEVICE_COMMAND";
     private static final String DEVICE_COMMAND_UPDATE = "DEVICE_COMMAND_UPDATE";
     private static final String DEVICE_NOTIFICATION = "DEVICE_NOTIFICATION";
-
     @EJB
     private HazelcastService hazelcastService;
-
     @EJB
     private LocalMessageBus localMessageBus;
-
     private HazelcastInstance hazelcast;
-
     private String commandListener;
     private String commandUpdateListener;
     private String notificationListener;
-
 
     @PostConstruct
     protected void postConstruct() {
@@ -76,7 +75,9 @@ public class GlobalMessageBus {
     }
 
     @Asynchronous
-    public void publishDeviceCommand(@Observes(during = TransactionPhase.AFTER_SUCCESS) @DeviceCommandService.Create DeviceCommand deviceCommand) {
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
+    public void publishDeviceCommand(@Observes(during = TransactionPhase.AFTER_SUCCESS) @DeviceCommandService.Create
+                                     DeviceCommand deviceCommand) {
         logger.debug("Sending device command {}", deviceCommand.getId());
         localMessageBus.submitDeviceCommand(deviceCommand);
         hazelcast.getTopic(DEVICE_COMMAND).publish(deviceCommand);
@@ -84,7 +85,10 @@ public class GlobalMessageBus {
     }
 
     @Asynchronous
-    public void publishDeviceCommandUpdate(@Observes(during = TransactionPhase.AFTER_SUCCESS) @DeviceCommandService.Update DeviceCommand deviceCommandUpdate) {
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
+    public void publishDeviceCommandUpdate(
+            @Observes(during = TransactionPhase.AFTER_SUCCESS) @DeviceCommandService.Update
+            DeviceCommand deviceCommandUpdate) {
         logger.debug("Sending device command update {}", deviceCommandUpdate.getId());
         localMessageBus.submitDeviceCommandUpdate(deviceCommandUpdate);
         hazelcast.getTopic(DEVICE_COMMAND_UPDATE).publish(deviceCommandUpdate);
@@ -92,15 +96,16 @@ public class GlobalMessageBus {
     }
 
     @Asynchronous
-    public void publishDeviceNotification(@Observes(during = TransactionPhase.AFTER_SUCCESS) DeviceNotification deviceNotification) {
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
+    public void publishDeviceNotification(
+            @Observes(during = TransactionPhase.AFTER_SUCCESS) DeviceNotification deviceNotification) {
         logger.debug("Sending device notification {}", deviceNotification.getId());
         localMessageBus.submitDeviceNotification(deviceNotification);
         hazelcast.getTopic(DEVICE_NOTIFICATION).publish(deviceNotification);
         logger.debug("Sent");
     }
 
-    private  class DeviceCommandListener implements MessageListener<DeviceCommand> {
-
+    private class DeviceCommandListener implements MessageListener<DeviceCommand> {
 
 
         @Override
@@ -115,7 +120,6 @@ public class GlobalMessageBus {
     private class DeviceCommandUpdateListener implements MessageListener<DeviceCommand> {
 
 
-
         @Override
         public void onMessage(Message<DeviceCommand> deviceCommandMessage) {
             if (!deviceCommandMessage.getPublishingMember().localMember()) {
@@ -125,8 +129,7 @@ public class GlobalMessageBus {
         }
     }
 
-
-    private  class DeviceNotificationListener implements MessageListener<DeviceNotification> {
+    private class DeviceNotificationListener implements MessageListener<DeviceNotification> {
 
 
         @Override
