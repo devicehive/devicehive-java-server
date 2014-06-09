29e4acce7f0619af9ce240551667c4728fc56889
jkulagina <Julia.Kulagina@dataart.com>
2014-06-09 18:57:39 +0400

    Configuration controller fix

--- server/src/main/java/com/devicehive/controller/ConfigurationController.java
+++ server/src/main/java/com/devicehive/controller/ConfigurationController.java
@@ -9,9 +9,12 @@ import org.slf4j.LoggerFactory;
 
 import javax.annotation.security.RolesAllowed;
 import javax.ejb.EJB;
-import javax.inject.Inject;
-import javax.validation.constraints.NotNull;
-import javax.ws.rs.*;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 
 import static com.devicehive.configuration.Constants.NAME;
@@ -24,45 +27,38 @@ import static com.devicehive.configuration.Constants.VALUE;
 @Path("/configuration")
 public class ConfigurationController {
     private static final Logger logger = LoggerFactory.getLogger(ConfigurationController.class);
-
     @EJB
     private ConfigurationService configurationService;
 
-
     @POST
     @RolesAllowed(HiveRoles.ADMIN)
-    @Path("/{"+NAME+"}")
-    public void setProperty(@PathParam(NAME) String name, String value) {
+    @Path("/set/{" + NAME + "}")
+    public Response setProperty(@PathParam(NAME) String name, String value) {
         configurationService.save(name, value);
+        return Response.ok().build();
     }
 
     @GET
     @RolesAllowed(HiveRoles.ADMIN)
-    @Path("/{"+NAME+"}")
-    public void setPropertyGet(@PathParam(NAME) String name, @QueryParam(VALUE) String value) {
+    @Path("/set/{" + NAME + "}")
+    public Response setPropertyGet(@PathParam(NAME) String name, @QueryParam(VALUE) String value) {
         configurationService.save(name, value);
+        return Response.ok().build();
     }
 
     @GET
     @RolesAllowed(HiveRoles.ADMIN)
-    @Path("/{"+NAME+"}")
+    @Path("/{" + NAME + "}")
     public String get(@PathParam(NAME) String name) {
         return configurationService.get(name);
     }
 
-
     @DELETE
     @RolesAllowed(HiveRoles.ADMIN)
-    @Path("/{"+NAME+"}")
+    @Path("/{" + NAME + "}")
     public Response deleteProperty(@PathParam(NAME) String name) {
-
+        //TODO
         return ResponseFactory.response(Response.Status.NO_CONTENT);
     }
 
-    @GET
-    @RolesAllowed(HiveRoles.ADMIN)
-    @Path("/reload")
-    public Response reloadConfig() {
-        return ResponseFactory.response(Response.Status.NO_CONTENT);
-    }
 }
