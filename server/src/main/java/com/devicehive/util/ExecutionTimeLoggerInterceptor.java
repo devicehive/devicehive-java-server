11647bcd081fb9a883bc2f162ba8df0d85b7144d
jkulagina <Julia.Kulagina@dataart.com>
2014-07-09 00:22:11 +0400

    LogExecutionTime disabled

--- server/src/main/java/com/devicehive/util/ExecutionTimeLoggerInterceptor.java
+++ server/src/main/java/com/devicehive/util/ExecutionTimeLoggerInterceptor.java
@@ -4,15 +4,12 @@ package com.devicehive.util;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.annotation.Priority;
 import javax.interceptor.AroundInvoke;
-import javax.interceptor.Interceptor;
 import javax.interceptor.InvocationContext;
 import java.lang.reflect.Method;
-import java.util.Calendar;
 
-@LogExecutionTime
-@Interceptor
+//@LogExecutionTime
+//@Interceptor
 public class ExecutionTimeLoggerInterceptor {
 
     @AroundInvoke
