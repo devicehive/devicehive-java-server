c475189a8ca31bcb6a227e73783ed4f2b9b039c8
ViktorKovrizhkin <vik.kovrizhkin@gmail.com>
2015-06-25 09:49:56 +0300

    admin-console removed

--- server-spring/src/main/java/com/devicehive/controller/WebController.java
+++ server-spring/src/main/java/com/devicehive/controller/WebController.java
@@ -1,11 +1,6 @@
 package com.devicehive.controller;
 
-import com.devicehive.configuration.ConfigurationService;
-import com.devicehive.configuration.Constants;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,21 +8,17 @@ public class WebController {
 
     @RequestMapping("/oauth2")
     public String oauth2() {
-        return "server/templates/oauth2/index";
+        return "templates/oauth2/index";
     }
 
     @RequestMapping("/login")
     public String oauth2Login() {
-        return "server/templates/oauthLogin/login";
+        return "templates/oauthLogin/login";
     }
 
     @RequestMapping("/home")
     public String oauth2Home() {
-        return "server/templates/oauthLogin/home";
+        return "templates/oauthLogin/home";
     }
 
-    @RequestMapping({"", "/admin"})
-    public String adminConsole() {
-        return "index";
-    }
 }
