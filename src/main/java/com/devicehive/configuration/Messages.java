e3d31fe88b8ca79dd6b3770ed0607702479fccc5
Mikhail Dolbin <lancemanfv@gmail.com>
2015-10-24 05:57:07 +0300

    #105 Deleting current user (used to authenticate current request) is forbidden now; Deleting user that owns current access key (used to authenticate current request)  is forbidden now; Response code 403 and user-friendly message will be returned in case of self-deletion attempts; Added new response message for this particular case.

--- src/main/java/com/devicehive/configuration/Messages.java
+++ src/main/java/com/devicehive/configuration/Messages.java
@@ -102,6 +102,7 @@ public class Messages {
     public static final String SHUTDOWN = BidBundle.get("SHUTDOWN");
     public static final String NO_ACCESS_TO_DEVICE = BidBundle.get("NO_ACCESS_TO_DEVICE");
     public static final String NO_NETWORKS_ASSIGNED_TO_USER = BidBundle.get("NO_NETWORKS_ASSIGNED_TO_USER");
+    public static final String CANT_DELETE_CURRENT_USER_KEY = BidBundle.get("CANT_DELETE_CURRENT_USER_KEY");
 
     /**
      * Bundle to extract localized strings from property files.
