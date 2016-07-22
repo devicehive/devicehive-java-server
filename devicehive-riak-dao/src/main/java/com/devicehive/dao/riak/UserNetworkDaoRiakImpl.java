3c72955d82a8a93ec04d1af400375989078f4f2a
Gleb <gleb.popov@dataart.com>
2016-07-22 17:18:04 +0300

    splitting on modules

---
+++ devicehive-riak-dao/src/main/java/com/devicehive/dao/riak/UserNetworkDaoRiakImpl.java
@@ -0,0 +1,100 @@
+package com.devicehive.dao.riak;
+
+
+import com.basho.riak.client.api.RiakClient;
+import com.basho.riak.client.api.commands.indexes.IntIndexQuery;
+import com.basho.riak.client.api.commands.kv.DeleteValue;
+import com.basho.riak.client.api.commands.kv.FetchValue;
+import com.basho.riak.client.api.commands.kv.StoreValue;
+import com.basho.riak.client.core.query.Location;
+import com.basho.riak.client.core.query.Namespace;
+import com.devicehive.exceptions.HivePersistenceLayerException;
+import com.devicehive.model.Network;
+import com.devicehive.model.UserNetwork;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ExecutionException;
+
+@Profile({"riak"})
+@Repository
+public class UserNetworkDaoRiakImpl extends RiakGenericDao {
+
+    private static final Namespace USER_NETWORK_NS = new Namespace("userNetwork");
+
+    @Autowired
+    private RiakClient client;
+
+    @Autowired
+    private RiakQuorum quorum;
+
+    public void persist(UserNetwork userNetwork) {
+        try {
+            String id = userNetwork.getUserId() + "n" + userNetwork.getNetworkId();
+            userNetwork.setId(id);
+            Location location = new Location(USER_NETWORK_NS, id);
+            StoreValue storeOp = new StoreValue.Builder(userNetwork)
+                    .withLocation(location)
+                    .withOption(quorum.getWriteQuorumOption(), quorum.getWriteQuorum())
+                    .build();
+            client.execute(storeOp);
+        } catch (ExecutionException | InterruptedException e) {
+            throw new HivePersistenceLayerException("Cannot persist user network.", e);
+        }
+    }
+
+    public UserNetwork merge(UserNetwork existing) {
+        try {
+            Location location = new Location(USER_NETWORK_NS, existing.getId());
+            StoreValue storeOp = new StoreValue.Builder(existing)
+                    .withLocation(location)
+                    .withOption(quorum.getWriteQuorumOption(), quorum.getWriteQuorum())
+                    .build();
+            client.execute(storeOp);
+            return existing;
+        } catch (ExecutionException | InterruptedException e) {
+            throw new HivePersistenceLayerException("Cannot merge user network.", e);
+        }
+    }
+
+    public void delete(long userId, long networkId) {
+        String id = userId + "n" + networkId;
+        Location location = new Location(USER_NETWORK_NS, id);
+        DeleteValue deleteOp = new DeleteValue.Builder(location).build();
+        try {
+            client.execute(deleteOp);
+        } catch (ExecutionException | InterruptedException e) {
+            throw new HivePersistenceLayerException("Cannot delete network.", e);
+        }
+    }
+
+    public Set<Long> findNetworksForUser(Long userId) {
+        IntIndexQuery biq = new IntIndexQuery.Builder(USER_NETWORK_NS, "userId", userId).withKeyAndIndex(true).build();
+        try {
+            IntIndexQuery.Response response = client.execute(biq);
+            List<UserNetwork> networkList = fetchMultiple(response, UserNetwork.class);
+            Set<Long> networks = new HashSet<>();
+            networkList.forEach(userNetwork -> networks.add(userNetwork.getNetworkId()));
+            return networks;
+        } catch (ExecutionException | InterruptedException e) {
+            throw new HivePersistenceLayerException("Cannot find networks for user.", e);
+        }
+    }
+
+    public Set<Long> findUsersInNetwork(Long networkId) {
+        IntIndexQuery biq = new IntIndexQuery.Builder(USER_NETWORK_NS, "networkId", networkId).build();
+        try {
+            IntIndexQuery.Response response = client.execute(biq);
+            List<UserNetwork> userNetworks = fetchMultiple(response, UserNetwork.class);
+            Set<Long> users = new HashSet<>();
+            userNetworks.forEach(userNetwork -> users.add(userNetwork.getUserId()));
+            return users;
+        } catch (ExecutionException | InterruptedException e) {
+            throw new HivePersistenceLayerException("Cannot find users in network.", e);
+        }
+    }
+}
---
+++ devicehive-services/src/test/java/com/devicehive/service/DeviceCommandServiceTest.java
@@ -0,0 +1,185 @@
+package com.devicehive.service;
+
+import com.devicehive.base.AbstractResourceTest;
+import com.devicehive.model.Device;
+import com.devicehive.model.DeviceCommand;
+import com.devicehive.model.JsonStringWrapper;
+import com.devicehive.model.User;
+import com.devicehive.model.wrappers.DeviceCommandWrapper;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.sql.Timestamp;
+import java.util.*;
+
+import static org.junit.Assert.*;
+
+public class DeviceCommandServiceTest extends AbstractResourceTest {
+    private static final String DEFAULT_STATUS = "default_status";
+    private static final Boolean DEFAULT_IS_UPDATED = false;
+
+    @Autowired
+    private DeviceCommandService deviceCommandService;
+
+    @Test
+    public void testFindAllCommands(){
+        final int NUMBER_OF_COMMANDS = 99;
+
+        for(int i = 0; i < NUMBER_OF_COMMANDS; i++){
+                sendNCommands(1, DEFAULT_STATUS, i % 2 == 0);
+        }
+
+        assertEquals(NUMBER_OF_COMMANDS, deviceCommandService.find(
+                null, Collections.<String>emptyList(), null, DEFAULT_STATUS, 100, null, null).size());
+    }
+
+    @Test
+    public void testFindCommandsWithResponse(){
+        final int NUMBER_OF_COMMANDS = 99;
+
+        int withResponse = 0;
+
+        for(int i = 0; i < NUMBER_OF_COMMANDS; i++){
+            if(i % 2 == 0) {
+                withResponse++;
+                sendNCommands(1, DEFAULT_STATUS, true);
+            }else{
+                sendNCommands(1, DEFAULT_STATUS, false);
+            }
+        }
+        assertEquals(withResponse, deviceCommandService.find(
+                null, Collections.<String>emptyList(), null, DEFAULT_STATUS, 100, true, null).size()
+                );
+    }
+    @Test
+    public void testFindCommandsWithoutResponse(){
+        final int NUMBER_OF_COMMANDS = 99;
+
+        int withResponse = 0;
+
+        for(int i = 0; i < NUMBER_OF_COMMANDS; i++){
+            if(i % 2 == 0) {
+                withResponse++;
+                sendNCommands(1, DEFAULT_STATUS, true);
+            }else{
+                sendNCommands(1, DEFAULT_STATUS, false);
+            }
+        }
+        assertEquals(NUMBER_OF_COMMANDS - withResponse, deviceCommandService.find(
+                null, Collections.<String>emptyList(), null, DEFAULT_STATUS, 100, false, null).size()
+                );
+    }
+
+
+    /**
+     * Simple test to check that all command were successfully saved and than retrieved
+     */
+    @Test
+    public void testSubmitDeviceCommands() {
+        final int NUMBER_OF_COMMANDS = 100;
+        sendNCommands(NUMBER_OF_COMMANDS);
+
+        final Collection<DeviceCommand> commands =  deviceCommandService.find(
+                null, Collections.<String>emptyList(), null, DEFAULT_STATUS, 100, false, null);
+        assertEquals(NUMBER_OF_COMMANDS, commands.size());
+    }
+
+    /**
+     * Tests all commands were saved with increasing timestamp
+     */
+    @Test
+    public void testIncreasingTimestamps() {
+        final int NUMBER_OF_COMMANDS = 20;
+        sendNCommands(NUMBER_OF_COMMANDS);
+
+        final List<DeviceCommand> commands =  new ArrayList<DeviceCommand>(deviceCommandService.find(
+                null, Collections.<String>emptyList(), null, DEFAULT_STATUS, 100, false, null));
+
+        for (int i = 1; i < commands.size(); i++) {
+            final Date currentElem = commands.get(i).getTimestamp();
+            final Date previousElem = commands.get(i - 1).getTimestamp();
+            assertTrue(currentElem.before(previousElem));
+        }
+    }
+
+    @Test
+    public void _testIncreasingTimestamps() {
+        final int NUMBER_OF_COMMANDS = 20;
+        sendNCommands(NUMBER_OF_COMMANDS);
+
+        final List<DeviceCommand> commands =  new ArrayList<DeviceCommand>(deviceCommandService.find(
+                null, Collections.<String>emptyList(), null, DEFAULT_STATUS, 100, true, null));
+
+        for (int i = 1; i < commands.size(); i++) {
+            final Date currentElem = commands.get(i).getTimestamp();
+            final Date previousElem = commands.get(i - 1).getTimestamp();
+            assertTrue(currentElem.before(previousElem));
+        }
+    }
+
+    /**
+     * Tests ability to retrieve commands which were after specified date
+     */
+    @Test
+    public void testRetrieveCommandsFromDate() {
+        final Timestamp timeBeforeBatches = new Timestamp(new Date().getTime());
+        sendNCommands(10);
+        try {
+            Thread.sleep(1000); //need it to have delay between two groups of commands
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        final Timestamp timeBetweenBatches = new Timestamp(new Date().getTime());
+        sendNCommands(15);
+
+        final Collection<DeviceCommand> commandsAll =  deviceCommandService.find(
+                null, Collections.<String>emptyList(), timeBeforeBatches, DEFAULT_STATUS, 100, false, null);
+        assertEquals(25, commandsAll.size());
+
+
+        final Collection<DeviceCommand> commands =  deviceCommandService.find(
+                null, Collections.<String>emptyList(), timeBetweenBatches, DEFAULT_STATUS, 100, false, null);
+        assertEquals(15, commands.size());
+
+    }
+
+    /**
+     * Tests ability to retrieve commands by name
+     */
+    @Test
+    public void testRetrieveCommandsByName() {
+        sendNCommands(10);
+
+        final Collection<DeviceCommand> commands =  deviceCommandService.find(
+                null, Arrays.asList("command2", "command3", "command4"), null, DEFAULT_STATUS, 100, false, null);
+        assertEquals(3, commands.size());
+    }
+
+    private void sendNCommands(int n, String status, boolean isUpdated) {
+        for (int i = 0; i < n; i++) {
+            //Need this hack to have different timestamp for each command
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            final DeviceCommand deviceCommand = new DeviceCommand();
+
+            deviceCommand.setId(Math.abs(new Random().nextInt()));
+            deviceCommand.setTimestamp(new Date());
+            deviceCommand.setUserId(0L);
+            deviceCommand.setDeviceGuid(UUID.randomUUID().toString());
+            deviceCommand.setCommand("command"+i);
+            deviceCommand.setParameters(new JsonStringWrapper("{'test':'test'}"));
+            deviceCommand.setStatus(status);
+            deviceCommand.setIsUpdated(isUpdated);
+
+            deviceCommandService.store(deviceCommand);
+        }
+    }
+
+    private void sendNCommands(int n) {
+        sendNCommands(n, DEFAULT_STATUS, DEFAULT_IS_UPDATED);
+    }
+}
