7f6ae796aeb2784f2606b446060685d5ee1eef63
Victor Kovryzhkin <vik.kovrizhkin@gmail.com>
2016-08-17 12:53:32 +0300

    kafka rpc-client and rpc-server spring profiles introduced

--- devicehive-shim-kafka-impl/src/main/java/com/devicehive/shim/config/client/KafkaRpcClientConfig.java
+++ devicehive-shim-kafka-impl/src/main/java/com/devicehive/shim/config/client/KafkaRpcClientConfig.java
@@ -20,6 +20,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 
@@ -29,6 +30,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 @Configuration
+@Profile("rpc-client")
 @PropertySource("classpath:kafka.properties")
 public class KafkaRpcClientConfig {
 
--- devicehive-shim-kafka-impl/src/main/java/com/devicehive/shim/config/server/KafkaRpcServerConfig.java
+++ devicehive-shim-kafka-impl/src/main/java/com/devicehive/shim/config/server/KafkaRpcServerConfig.java
@@ -17,6 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 
@@ -25,6 +26,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 @Configuration
+@Profile("rpc-server")
 @PropertySource("classpath:kafka.properties")
 public class KafkaRpcServerConfig {
 
