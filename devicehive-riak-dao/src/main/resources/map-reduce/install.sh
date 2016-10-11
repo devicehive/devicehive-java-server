cf55b73574bfcefdc274e618d7818eee4ae7ae5c
Denis Dechev <denias@ukr.net>
2016-10-11 16:34:23 +0300

    fixes for travis

--- devicehive-riak-dao/src/main/resources/map-reduce/install.sh
+++ devicehive-riak-dao/src/main/resources/map-reduce/install.sh
@@ -1,5 +1,6 @@
-!#/bin/bash
+#!/bin/bash
 mkdir -p /etc/riak/dh-mr/src/
-cp *.erl /etc/riak/dh-mr/src/
+mkdir -p /etc/riak/dh-mr/ebin/
+cp devicehive-riak-dao/src/main/resources/map-reduce/*.erl /etc/riak/dh-mr/src/
 /usr/lib/riak/$(ls /usr/lib/riak/ | grep erts)/bin/erlc -o /etc/riak/dh-mr/ebin /etc/riak/dh-mr/src/*.erl
-echo '[{riak_kv, [{add_paths, ["/etc/riak/dh-mr/ebin"]}]}].' > /etc/riak
+echo '[{riak_kv, [{add_paths, ["/etc/riak/dh-mr/ebin"]}]}].' > /etc/riak/advanced.config
