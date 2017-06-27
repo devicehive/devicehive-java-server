1c6603eb591ac0844292c06ab95aad8375aebead
yury.kitsenko <yury.kitsenko@dataart.com>
2017-06-27 17:11:42 +0300

    Rename "user" table tests fixed

--- devicehive-frontend/src/test/resources/initial_data.sql
+++ devicehive-frontend/src/test/resources/initial_data.sql
@@ -19,7 +19,7 @@
 ---
 -- 1. Default users
 -- admin -> test_admin : admin_pass
-INSERT INTO "user" (login, password_hash, password_salt, role, status, login_attempts) VALUES ('test_admin', '+IC4w+NeByiymEWlI5H1xbtNe4YKmPlLRZ7j3xaireg=', '9KynX3ShWnFym4y8Dla039py', 0, 0, 0);
+INSERT INTO "dh_user" (login, password_hash, password_salt, role, status, login_attempts) VALUES ('test_admin', '+IC4w+NeByiymEWlI5H1xbtNe4YKmPlLRZ7j3xaireg=', '9KynX3ShWnFym4y8Dla039py', 0, 0, 0);
 
 INSERT INTO configuration (name, value, entity_version) VALUES ('session.timeout', '1200000', 0);
 INSERT INTO configuration (name, value, entity_version) VALUES ('allowNetworkAutoCreate', 'true', 0);
