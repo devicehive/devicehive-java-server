77047eb9e4156b576b0eed9771f43b858e4f64ec
Alexander Sitnik <alex.sitnik@gmail.com>
2017-07-25 19:37:32 +0300

    [Docker] Add new startup parameters

--- dockerfiles/devicehive-backend-riak/devicehive-start.sh
+++ dockerfiles/devicehive-backend-riak/devicehive-start.sh
@@ -112,13 +112,21 @@ curl -XPUT \
 
 echo "Starting DeviceHive backend"
 java -server -Xms1g -Xmx2g -XX:+UseG1GC -XX:MaxGCPauseMillis=20 -XX:+DisableExplicitGC -XX:+HeapDumpOnOutOfMemoryError -jar \
+-Dacks=${DH_ACKS:-1} \
+-Dauto.commit.interval.ms=${DH_AUTO_COMMIT_INTERVAL_MS:-5000} \
 -Dcom.devicehive.log.level=${DH_LOG_LEVEL:-WARN} \
+-Denable.auto.commit=${DH_ENABLE_AUTO_COMMIT:-true} \
+-Dfetch.max.wait.ms=${DH_FETCH_MAX_WAIT_MS:-100} \
+-Dfetch.min.bytes=${DH_FETCH_MIN_BYTES:-1} \
+-Dreplication.factor=${DH_REPLICATION_FACTOR:-1} \
 -Droot.log.level=${ROOT_LOG_LEVEL:-WARN} \
 -Dflyway.enabled=false \
 -Driak.host=${DH_RIAK_HOST} \
 -Driak.port=${DH_RIAK_PORT} \
 -Dbootstrap.servers=${DH_KAFKA_ADDRESS}:${DH_KAFKA_PORT} \
 -Dzookeeper.connect=${DH_ZK_ADDRESS}:${DH_ZK_PORT} \
+-Dzookeeper.connectionTimeout=${DH_ZK_CONNECTIONTIMEOUT:-8000} \
+-Dzookeeper.sessionTimeout=${DH_ZK_SESSIONTIMEOUT:-10000} \
 -Dhazelcast.cluster.members=${HC_MEMBERS}:${HC_PORT} \
 -Dhazelcast.group.name=${HC_GROUP_NAME} \
 -Dhazelcast.group.password=${HC_GROUP_PASSWORD} \
