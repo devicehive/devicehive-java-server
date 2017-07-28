870bac0bd4f73ec11058f4d56d99402a7d925b05
Alexander Sitnik <alex.sitnik@gmail.com>
2017-07-28 13:47:11 +0300

    [Docker] Fix what shellcheck found in init scripts
    Mainly by quoting all variables

--- dockerfiles/devicehive-backend-rdbms/devicehive-start.sh
+++ dockerfiles/devicehive-backend-rdbms/devicehive-start.sh
@@ -6,8 +6,8 @@ trap 'terminate' TERM INT
 
 terminate() {
    echo "SIGTERM received, terminating $PID"
-   kill -TERM $PID
-   wait $PID
+   kill -TERM "$PID"
+   wait "$PID"
 }
 
 # Check if all required parameters are set
@@ -45,17 +45,17 @@ fi
 
 # Check if Zookeper, Kafka and Postgres are ready
 while true; do
-    nc -v -z -w1 $DH_ZK_ADDRESS ${DH_ZK_PORT:=2181}
+    nc -v -z -w1 "$DH_ZK_ADDRESS" "${DH_ZK_PORT:=2181}"
     result_zk=$?
     FIRST_KAFKA_SERVER="${DH_KAFKA_BOOTSTRAP_SERVERS%%,*}"
-    nc -v -z -w1 ${FIRST_KAFKA_SERVER%%:*} $(expr $FIRST_KAFKA_SERVER : '.*:\([0-9]*\)')
+    nc -v -z -w1 "${FIRST_KAFKA_SERVER%%:*}" $(expr $FIRST_KAFKA_SERVER : '.*:\([0-9]*\)')
     result_kafka=$?
-    nc -v -z -w1 $DH_POSTGRES_ADDRESS ${DH_POSTGRES_PORT:=5432}
+    nc -v -z -w1 "$DH_POSTGRES_ADDRESS" "${DH_POSTGRES_PORT:=5432}"
     result_postgres=$?
-    nc -v -z -w1 ${HC_MEMBERS%%,*} ${HC_PORT:=5701}
+    nc -v -z -w1 "${HC_MEMBERS%%,*}" "${HC_PORT:=5701}"
     result_hc=$?
 
-    if [ "$result_kafka" -eq 0 -a "$result_postgres" -eq 0 -a "$result_zk" -eq 0 -a "$result_hc" -eq 0 ]; then
+    if [ "$result_kafka" -eq 0 ] && [ "$result_postgres" -eq 0 ] && [ "$result_zk" -eq 0 ] && [ "$result_hc" -eq 0 ]; then
         break
     fi
     sleep 3
@@ -63,27 +63,27 @@ done
 
 echo "Starting DeviceHive backend"
 java -server -Xms1g -Xmx2g -XX:+UseG1GC -XX:MaxGCPauseMillis=20 -XX:+DisableExplicitGC -XX:+HeapDumpOnOutOfMemoryError -jar \
--Dacks=${DH_ACKS:-1} \
--Dauto.commit.interval.ms=${DH_AUTO_COMMIT_INTERVAL_MS:-5000} \
--Dbatch.size=${DH_BATCH_SIZE:-98304} \
--Dbootstrap.servers=${DH_KAFKA_BOOTSTRAP_SERVERS} \
--Dcom.devicehive.log.level=${DH_LOG_LEVEL:-WARN} \
--Denable.auto.commit=${DH_ENABLE_AUTO_COMMIT:-true} \
--Dfetch.max.wait.ms=${DH_FETCH_MAX_WAIT_MS:-100} \
--Dfetch.min.bytes=${DH_FETCH_MIN_BYTES:-1} \
--Dhazelcast.cluster.members=${HC_MEMBERS}:${HC_PORT} \
--Dhazelcast.group.name=${HC_GROUP_NAME} \
--Dhazelcast.group.password=${HC_GROUP_PASSWORD} \
--Dreplication.factor=${DH_REPLICATION_FACTOR:-1} \
--Droot.log.level=${ROOT_LOG_LEVEL:-WARN} \
--Drpc.server.request-consumer.threads=${DH_RPC_SERVER_REQ_CONS_THREADS:-3} \
--Drpc.server.worker.threads=${DH_RPC_SERVER_WORKER_THREADS:-3} \
--Dspring.datasource.url=jdbc:postgresql://${DH_POSTGRES_ADDRESS}:${DH_POSTGRES_PORT}/${DH_POSTGRES_DB} \
+-Dacks="${DH_ACKS:-1}" \
+-Dauto.commit.interval.ms="${DH_AUTO_COMMIT_INTERVAL_MS:-5000}" \
+-Dbatch.size="${DH_BATCH_SIZE:-98304}" \
+-Dbootstrap.servers="${DH_KAFKA_BOOTSTRAP_SERVERS}" \
+-Dcom.devicehive.log.level="${DH_LOG_LEVEL:-WARN}" \
+-Denable.auto.commit="${DH_ENABLE_AUTO_COMMIT:-true}" \
+-Dfetch.max.wait.ms="${DH_FETCH_MAX_WAIT_MS:-100}" \
+-Dfetch.min.bytes="${DH_FETCH_MIN_BYTES:-1}" \
+-Dhazelcast.cluster.members="${HC_MEMBERS}:${HC_PORT}" \
+-Dhazelcast.group.name="${HC_GROUP_NAME}" \
+-Dhazelcast.group.password="${HC_GROUP_PASSWORD}" \
+-Dreplication.factor="${DH_REPLICATION_FACTOR:-1}" \
+-Droot.log.level="${ROOT_LOG_LEVEL:-WARN}" \
+-Drpc.server.request-consumer.threads="${DH_RPC_SERVER_REQ_CONS_THREADS:-3}" \
+-Drpc.server.worker.threads="${DH_RPC_SERVER_WORKER_THREADS:-3}" \
+-Dspring.datasource.url="jdbc:postgresql://${DH_POSTGRES_ADDRESS}:${DH_POSTGRES_PORT}/${DH_POSTGRES_DB}" \
 -Dspring.datasource.username="${DH_POSTGRES_USERNAME}" \
 -Dspring.datasource.password="${DH_POSTGRES_PASSWORD}" \
--Dzookeeper.connect=${DH_ZK_ADDRESS}:${DH_ZK_PORT} \
--Dzookeeper.connectionTimeout=${DH_ZK_CONNECTIONTIMEOUT:-8000} \
--Dzookeeper.sessionTimeout=${DH_ZK_SESSIONTIMEOUT:-10000} \
-./devicehive-backend-${DH_VERSION}-boot.jar &
+-Dzookeeper.connect="${DH_ZK_ADDRESS}:${DH_ZK_PORT}" \
+-Dzookeeper.connectionTimeout="${DH_ZK_CONNECTIONTIMEOUT:-8000}" \
+-Dzookeeper.sessionTimeout="${DH_ZK_SESSIONTIMEOUT:-10000}" \
+"./devicehive-backend-${DH_VERSION}-boot.jar" &
 PID=$!
 wait $PID
