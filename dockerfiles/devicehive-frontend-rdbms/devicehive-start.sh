4f746a18a21442b4eea5cf6c8d09588856fc7b57
Alexander Sitnik <alex.sitnik@gmail.com>
2017-07-28 13:47:11 +0300

    [Docker] Fix what shellcheck found in init scripts
    Mainly by quoting all variables

--- dockerfiles/devicehive-frontend-rdbms/devicehive-start.sh
+++ dockerfiles/devicehive-frontend-rdbms/devicehive-start.sh
@@ -6,8 +6,8 @@ trap 'terminate' TERM INT
 
 terminate() {
    echo "SIGTERM received, terminating $PID"
-   kill -TERM $PID
-   wait $PID
+   kill -TERM "$PID"
+   wait "$PID"
 }
 
 # Check if all required parameters are set
@@ -39,25 +39,25 @@ fi
 
 echo "Starting DeviceHive frontend"
 java -server -Xms1g -Xmx2g -XX:+UseG1GC -XX:MaxGCPauseMillis=20 -XX:+DisableExplicitGC -XX:+HeapDumpOnOutOfMemoryError -jar \
--Dacks=${DH_ACKS:-1} \
--Dauto.commit.interval.ms=${DH_AUTO_COMMIT_INTERVAL_MS:-5000} \
--Dbatch.size=${DH_BATCH_SIZE:-98304} \
--Dbootstrap.servers=${DH_KAFKA_BOOTSTRAP_SERVERS} \
--Dcom.devicehive.log.level=${DH_LOG_LEVEL:-WARN} \
--Denable.auto.commit=${DH_ENABLE_AUTO_COMMIT:-true} \
--Dfetch.max.wait.ms=${DH_FETCH_MAX_WAIT_MS:-100} \
--Dfetch.min.bytes=${DH_FETCH_MIN_BYTES:-1} \
--Dreplication.factor=${DH_REPLICATION_FACTOR:-1} \
--Droot.log.level=${ROOT_LOG_LEVEL:-WARN} \
--Drpc.client.response-consumer.threads=${DH_RPC_CLIENT_RES_CONS_THREADS:-3} \
+-Dacks="${DH_ACKS:-1}" \
+-Dauto.commit.interval.ms="${DH_AUTO_COMMIT_INTERVAL_MS:-5000}" \
+-Dbatch.size="${DH_BATCH_SIZE:-98304}" \
+-Dbootstrap.servers="${DH_KAFKA_BOOTSTRAP_SERVERS}" \
+-Dcom.devicehive.log.level="${DH_LOG_LEVEL:-WARN}" \
+-Denable.auto.commit="${DH_ENABLE_AUTO_COMMIT:-true}" \
+-Dfetch.max.wait.ms="${DH_FETCH_MAX_WAIT_MS:-100}" \
+-Dfetch.min.bytes="${DH_FETCH_MIN_BYTES:-1}" \
+-Dreplication.factor="${DH_REPLICATION_FACTOR:-1}" \
+-Droot.log.level="${ROOT_LOG_LEVEL:-WARN}" \
+-Drpc.client.response-consumer.threads="${DH_RPC_CLIENT_RES_CONS_THREADS:-3}" \
 -Dserver.context-path=/api \
 -Dserver.port=8080 \
--Dspring.datasource.url=jdbc:postgresql://${DH_POSTGRES_ADDRESS}:${DH_POSTGRES_PORT:-5432}/${DH_POSTGRES_DB} \
+-Dspring.datasource.url="jdbc:postgresql://${DH_POSTGRES_ADDRESS}:${DH_POSTGRES_PORT:-5432}/${DH_POSTGRES_DB}" \
 -Dspring.datasource.username="${DH_POSTGRES_USERNAME}" \
 -Dspring.datasource.password="${DH_POSTGRES_PASSWORD}" \
--Dzookeeper.connect=${DH_ZK_ADDRESS}:${DH_ZK_PORT:-2181} \
--Dzookeeper.connectionTimeout=${DH_ZK_CONNECTIONTIMEOUT:-8000} \
--Dzookeeper.sessionTimeout=${DH_ZK_SESSIONTIMEOUT:-10000} \
-./devicehive-frontend-${DH_VERSION}-boot.jar &
+-Dzookeeper.connect="${DH_ZK_ADDRESS}:${DH_ZK_PORT:-2181}" \
+-Dzookeeper.connectionTimeout="${DH_ZK_CONNECTIONTIMEOUT:-8000}" \
+-Dzookeeper.sessionTimeout="${DH_ZK_SESSIONTIMEOUT:-10000}" \
+"./devicehive-frontend-${DH_VERSION}-boot.jar" &
 PID=$!
-wait $PID
+wait "$PID"
--- dockerfiles/devicehive-frontend-riak/devicehive-start.sh
+++ dockerfiles/devicehive-frontend-riak/devicehive-start.sh
@@ -6,8 +6,8 @@ trap 'terminate' TERM INT
 
 terminate() {
    echo "SIGTERM received, terminating $PID"
-   kill -TERM $PID
-   wait $PID
+   kill -TERM "$PID"
+   wait "$PID"
 }
 
 # Check if all required parameters are set
@@ -35,25 +35,25 @@ fi
 
 echo "Starting DeviceHive frontend"
 java -server -Xms1g -Xmx2g -XX:+UseG1GC -XX:MaxGCPauseMillis=20 -XX:+DisableExplicitGC -XX:+HeapDumpOnOutOfMemoryError -jar \
--Dacks=${DH_ACKS:-1} \
--Dauto.commit.interval.ms=${DH_AUTO_COMMIT_INTERVAL_MS:-5000} \
--Dbatch.size=${DH_BATCH_SIZE:-98304} \
--Dbootstrap.servers=${DH_KAFKA_BOOTSTRAP_SERVERS} \
--Dcom.devicehive.log.level=${DH_LOG_LEVEL:-WARN} \
--Denable.auto.commit=${DH_ENABLE_AUTO_COMMIT:-true} \
--Dfetch.max.wait.ms=${DH_FETCH_MAX_WAIT_MS:-100} \
--Dfetch.min.bytes=${DH_FETCH_MIN_BYTES:-1} \
+-Dacks="${DH_ACKS:-1}" \
+-Dauto.commit.interval.ms="${DH_AUTO_COMMIT_INTERVAL_MS:-5000}" \
+-Dbatch.size="${DH_BATCH_SIZE:-98304}" \
+-Dbootstrap.servers="${DH_KAFKA_BOOTSTRAP_SERVERS}" \
+-Dcom.devicehive.log.level="${DH_LOG_LEVEL:-WARN}" \
+-Denable.auto.commit="${DH_ENABLE_AUTO_COMMIT:-true}" \
+-Dfetch.max.wait.ms="${DH_FETCH_MAX_WAIT_MS:-100}" \
+-Dfetch.min.bytes="${DH_FETCH_MIN_BYTES:-1}" \
 -Dflyway.enabled=false \
--Dreplication.factor=${DH_REPLICATION_FACTOR:-1} \
--Driak.host=${DH_RIAK_HOST} \
--Driak.port=${DH_RIAK_PORT} \
--Droot.log.level=${ROOT_LOG_LEVEL:-WARN} \
--Drpc.client.response-consumer.threads=${DH_RPC_CLIENT_RES_CONS_THREADS:-3} \
+-Dreplication.factor="${DH_REPLICATION_FACTOR:-1}" \
+-Driak.host="${DH_RIAK_HOST}" \
+-Driak.port="${DH_RIAK_PORT}" \
+-Droot.log.level="${ROOT_LOG_LEVEL:-WARN}" \
+-Drpc.client.response-consumer.threads="${DH_RPC_CLIENT_RES_CONS_THREADS:-3}" \
 -Dserver.context-path=/api \
 -Dserver.port=8080 \
--Dzookeeper.connect=${DH_ZK_ADDRESS}:${DH_ZK_PORT:-2181} \
--Dzookeeper.connectionTimeout=${DH_ZK_CONNECTIONTIMEOUT:-8000} \
--Dzookeeper.sessionTimeout=${DH_ZK_SESSIONTIMEOUT:-10000} \
-./devicehive-frontend-${DH_VERSION}-boot.jar &
+-Dzookeeper.connect="${DH_ZK_ADDRESS}:${DH_ZK_PORT:-2181}" \
+-Dzookeeper.connectionTimeout="${DH_ZK_CONNECTIONTIMEOUT:-8000}" \
+-Dzookeeper.sessionTimeout="${DH_ZK_SESSIONTIMEOUT:-10000}" \
+"./devicehive-frontend-${DH_VERSION}-boot.jar" &
 PID=$!
-wait $PID
+wait "$PID"
