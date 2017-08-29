97d57fb2afa126803dd8eaff122c06c2febc4c18
Alexander Sitnik <alex.sitnik@gmail.com>
2017-08-29 16:54:09 +0300

    docker: add ExitOnOutOfMemoryError to startup scripts

--- dockerfiles/devicehive-frontend-rdbms/devicehive-start.sh
+++ dockerfiles/devicehive-frontend-rdbms/devicehive-start.sh
@@ -38,7 +38,7 @@ then
 fi
 
 echo "Starting DeviceHive frontend"
-java -server -Xms1g -Xmx2g -XX:+UseG1GC -XX:MaxGCPauseMillis=20 -XX:+DisableExplicitGC -XX:+HeapDumpOnOutOfMemoryError -jar \
+java -server -Xms1g -Xmx2g -XX:+UseG1GC -XX:MaxGCPauseMillis=20 -XX:+DisableExplicitGC -XX:+HeapDumpOnOutOfMemoryError -XX:+ExitOnOutOfMemoryError -jar \
 -Dacks="${DH_ACKS:-1}" \
 -Dauto.commit.interval.ms="${DH_AUTO_COMMIT_INTERVAL_MS:-5000}" \
 -Dbatch.size="${DH_BATCH_SIZE:-98304}" \
