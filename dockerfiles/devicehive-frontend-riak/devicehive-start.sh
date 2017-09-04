8fb7f6fd3f6cfefc930a8a10734d888636176e81
Alexander Sitnik <alex.sitnik@gmail.com>
2017-09-04 16:04:13 +0300

    docker: add Hazelcast parameters to FE init script

--- dockerfiles/devicehive-frontend-riak/devicehive-start.sh
+++ dockerfiles/devicehive-frontend-riak/devicehive-start.sh
@@ -13,6 +13,9 @@ terminate() {
 # Check if all required parameters are set
 if [ -z "$DH_RIAK_HOST" ] \
     || [ -z "$DH_RIAK_PORT" ] \
+    || [ -z "$HC_MEMBERS" ] \
+    || [ -z "$HC_GROUP_NAME" ] \
+    || [ -z "$HC_GROUP_PASSWORD" ] \
     || [ -z "$DH_ZK_ADDRESS" ] \
     || ( [ -z "$DH_KAFKA_BOOTSTRAP_SERVERS" ] && [ -z "$DH_KAFKA_ADDRESS" ] )
 then
@@ -20,6 +23,9 @@ then
     echo "Please check following vars are passed to container:"
     echo "- DH_RIAK_HOST"
     echo "- DH_RIAK_PORT"
+    echo "- HC_MEMBERS"
+    echo "- HC_GROUP_NAME"
+    echo "- HC_GROUP_PASSWORD"
     echo "- DH_ZK_ADDRESS"
     echo "And one of variants of Kafka bootstrap parameters:"
     echo "- DH_KAFKA_BOOTSTRAP_SERVERS for multiple servers"
@@ -33,6 +39,24 @@ then
     DH_KAFKA_BOOTSTRAP_SERVERS="${DH_KAFKA_ADDRESS}:${DH_KAFKA_PORT:-9092}"
 fi
 
+# Check if Zookeper, Kafka, Riak and Hazelcast are ready
+while true; do
+    nc -v -z -w1 "$DH_ZK_ADDRESS" "${DH_ZK_PORT:=2181}"
+    result_zk=$?
+    FIRST_KAFKA_SERVER="${DH_KAFKA_BOOTSTRAP_SERVERS%%,*}"
+    nc -v -z -w1 "${FIRST_KAFKA_SERVER%%:*}" $(expr $FIRST_KAFKA_SERVER : '.*:\([0-9]*\)')
+    result_kafka=$?
+    curl --output /dev/null --silent --head --fail "http://${DH_RIAK_HOST_MEMBER}:${DH_RIAK_HTTP_PORT}/ping"
+    result_riak=$?
+    nc -v -z -w1 "${HC_MEMBERS%%,*}" "${HC_PORT:=5701}"
+    result_hc=$?
+
+    if [ "$result_kafka" -eq 0 ] && [ "$result_riak" -eq 0 ] && [ "$result_zk" -eq 0 ] && [ "$result_hc" -eq 0 ]; then
+        break
+    fi
+    sleep 3
+done
+
 echo "Starting DeviceHive frontend"
 java -server -Xms1g -Xmx2g -XX:+UseG1GC -XX:MaxGCPauseMillis=20 -XX:+DisableExplicitGC -XX:+HeapDumpOnOutOfMemoryError -XX:+ExitOnOutOfMemoryError -jar \
 -Dacks="${DH_ACKS:-1}" \
@@ -43,6 +67,9 @@ java -server -Xms1g -Xmx2g -XX:+UseG1GC -XX:MaxGCPauseMillis=20 -XX:+DisableExpl
 -Denable.auto.commit="${DH_ENABLE_AUTO_COMMIT:-true}" \
 -Dfetch.max.wait.ms="${DH_FETCH_MAX_WAIT_MS:-100}" \
 -Dfetch.min.bytes="${DH_FETCH_MIN_BYTES:-1}" \
+-Dhazelcast.cluster.members="${HC_MEMBERS}:${HC_PORT}" \
+-Dhazelcast.group.name="${HC_GROUP_NAME}" \
+-Dhazelcast.group.password="${HC_GROUP_PASSWORD}" \
 -Dflyway.enabled=false \
 -Dreplication.factor="${DH_REPLICATION_FACTOR:-1}" \
 -Driak.host="${DH_RIAK_HOST}" \
