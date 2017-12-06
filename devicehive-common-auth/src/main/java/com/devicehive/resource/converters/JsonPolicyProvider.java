11c8cf9f7f2636e3a31b44f2017a45d6eb36ee83
Andrey Latysh <alatysh@dataart.com>
2017-12-06 14:05:13 +0200

    Fix: Multiple exception in Frontend log; https://support.dataart.com/browse/DEV-343; Exception handling and logging added.

--- devicehive-common-auth/src/main/java/com/devicehive/resource/converters/JsonPolicyProvider.java
+++ devicehive-common-auth/src/main/java/com/devicehive/resource/converters/JsonPolicyProvider.java
@@ -26,6 +26,9 @@ import com.devicehive.json.strategies.JsonPolicyApply;
 import com.devicehive.json.strategies.JsonPolicyDef;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonIOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
@@ -46,6 +49,7 @@ import java.nio.charset.Charset;
 import static com.devicehive.configuration.Constants.UTF8;
 
 public abstract class JsonPolicyProvider<T> implements MessageBodyWriter<T>, MessageBodyReader<T> {
+    private static final Logger logger = LoggerFactory.getLogger(JsonPolicyProvider.class);
 
     @Override
     public boolean isWriteable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
@@ -62,16 +66,22 @@ public abstract class JsonPolicyProvider<T> implements MessageBodyWriter<T>, Mes
     public void writeTo(T entity, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType,
                         MultivaluedMap<String, Object> httpHeaders, OutputStream entityStream)
             throws IOException, WebApplicationException {
-        Gson gson = createGson(annotations);
-        JsonElement jsonElement = gson.toJsonTree(entity);
-        Writer writer = null;
         try {
-            writer = new OutputStreamWriter(entityStream, Charset.forName(UTF8));
-            gson.toJson(jsonElement, writer);
-        } finally {
-            if (writer != null) {
-                writer.flush();
+            Gson gson = createGson(annotations);
+            JsonElement jsonElement = gson.toJsonTree(entity);
+            Writer writer = null;
+            try {
+                writer = new OutputStreamWriter(entityStream, Charset.forName(UTF8));
+                gson.toJson(jsonElement, writer);
+            } finally {
+                if (writer != null) {
+                    writer.flush();
+                }
             }
+        } catch (JsonIOException e) {
+            logger.debug("Experiencing issues on response parsing.");
+        } catch (IOException e) {
+            logger.debug("Experiencing issues on response output.");
         }
     }
 
