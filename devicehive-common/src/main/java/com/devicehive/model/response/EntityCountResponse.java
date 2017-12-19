850684f39317086ece3919f99ba2acc93b71bafc
Andrey Latysh <alatysh@dataart.com>
2017-12-19 14:19:56 +0200

    Feature: Frontend: Implement /user/count api endpoint; https://support.dataart.com/browse/DEV-368; /user/count via HTTP implemented.

---
+++ devicehive-common/src/main/java/com/devicehive/model/response/EntityCountResponse.java
@@ -0,0 +1,48 @@
+package com.devicehive.model.response;
+
+/*
+ * #%L
+ * DeviceHive Common Dao Interfaces
+ * %%
+ * Copyright (C) 2016 DataArt
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import com.devicehive.json.strategies.JsonPolicyDef;
+import com.devicehive.model.HiveEntity;
+import com.google.gson.annotations.SerializedName;
+
+import static com.devicehive.json.strategies.JsonPolicyDef.Policy.ENTITIES_COUNTED;
+
+public class EntityCountResponse implements HiveEntity {
+
+    private static final long serialVersionUID = 5041089226691783525L;
+
+    @SerializedName("count")
+    @JsonPolicyDef({ENTITIES_COUNTED})
+    private long count;
+
+    public EntityCountResponse(long count) {
+        this.count = count;
+    }
+
+    public long getCount() {
+        return count;
+    }
+
+    public void setCount(long count) {
+        this.count = count;
+    }
+}
