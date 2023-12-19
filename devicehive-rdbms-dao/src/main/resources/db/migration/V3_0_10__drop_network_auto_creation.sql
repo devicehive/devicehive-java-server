93e43cf24a0f02f65e2f00c4fa2709e47e6e889e
Aleksandar Osenov <aleksandar.osenov@dataart.com>
2023-12-19 10:56:59 +0200

    Fixed copyright header

--- devicehive-rdbms-dao/src/main/resources/db/migration/V3_0_10__drop_network_auto_creation.sql
+++ devicehive-rdbms-dao/src/main/resources/db/migration/V3_0_10__drop_network_auto_creation.sql
@@ -2,7 +2,7 @@
 -- #%L
 -- DeviceHive Dao RDBMS Implementation
 -- %%
--- Copyright (C) 2016-2017 DataArt
+-- Copyright (C) 2016 DataArt
 -- %%
 -- Licensed under the Apache License, Version 2.0 (the "License");
 -- you may not use this file except in compliance with the License.
